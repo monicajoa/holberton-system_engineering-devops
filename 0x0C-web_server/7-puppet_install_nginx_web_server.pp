af961d9c8f40d883f67b22e461210a7ff0cca2e2
monicajoa <mnortiz.ortiz@gmail.com>
2020-09-03 02:40:46 -0500

    web server project, avanced task 7 puppet, review the task, rewrite the code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,5 @@
 # Install Nginx web server (w/ Puppet)
+
 package { 'nginx':
   ensure   => 'latest',
   name     => 'nginx',
@@ -7,11 +8,11 @@ package { 'nginx':
 
 file { 'index':
   path    => '/var/www/html/index.html',
-  mode    => '0644',
+  mode    => '0664',
   content => 'Holberton School'
 }
 
-file_line { 'redirect_301':
+file_line { 'redirect_me':
   path  => '/etc/nginx/sites-available/default',
   line  => "\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}",
   after => '^server {',
